366e7e35bf66fc2e1a86b8f41ad06dfe1297389d
Salazar <154253650+Salazar-the-Terrible@users.noreply.github.com>
2026-02-06 17:46:46 +0330

    CNN without canny

---
+++ src/cnn_train.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import os
+
+from PIL import Image
+from torchvision.datasets import ImageFolder
+
+IMG_SIZE = 64
+BATCH_SIZE = 32
+EPOCHS = 15
+DATA_DIR = "data"
+
+transform = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.ToTensor(),
+])
+
+class SafeImageFolder(ImageFolder):
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        try:
+            sample = self.loader(path)
+        except Exception:
+            return self.__getitem__((index + 1) % len(self.samples))
+
+        if self.transform is not None:
+            sample = self.transform(sample)
+
+        return sample, target
+
+dataset = SafeImageFolder(
+    root=DATA_DIR,
+    transform=transform
+)
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+
+train_set, test_set = torch.utils.data.random_split(
+    dataset, [train_size, test_size]
+)
+
+train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=BATCH_SIZE)
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.fc1 = nn.Linear(64 * 6 * 6, 64)
+        self.fc2 = nn.Linear(64, 2)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = SimpleCNN()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(EPOCHS):
+    running_loss = 0.0
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+
+    epoch_loss = running_loss / len(train_loader)
+    print(f"Epoch {epoch+1}, Avg loss per batch: {epoch_loss:.4f}")
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print("Test accuracy:", correct / total)
+torch.save(model.state_dict(), "cnn_cat_dog.pth")
