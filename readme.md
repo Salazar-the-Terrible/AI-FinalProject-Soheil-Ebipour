3e5a54b73b5fb4ec7c65d90d99b7dac9e0e78455
Salazar <154253650+Salazar-the-Terrible@users.noreply.github.com>
2026-02-14 09:12:06 +0330

    Readme, also removed unnecessary files as per project guide

---
+++ readme.md
@@ -0,0 +1,6 @@
+This project aims to produce a model that can tell apart cat images from dog images. To achieve this the kaggle "cat vs dog" dataset was used. The project uses a 5 layer convolutional neural network(cnn) to achieve its goal.
+
+Training images should be placed in the data folder and be divided into 2 subfolders named "cat" and "dog".
+
+To run the training, simply run the cnn_train script.
+To test, run the ui.py script.
--- src/classic-cnn-train.py
+++ src/classic-cnn-train.py
@@ -11,9 +11,9 @@ from torchvision.datasets import ImageFolder
 import cv2
 import numpy as np
 
-IMG_SIZE = 64
+IMG_SIZE = 128
 BATCH_SIZE = 32
-EPOCHS = 10
+EPOCHS = 15
 DATA_DIR = "data"
 
 class CannyTransform:
@@ -58,20 +58,32 @@ test_loader = DataLoader(test_set, batch_size=BATCH_SIZE)
 class SimpleCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(1,   16,  3, padding=1)
+        self.bn1   = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16,  32,  3, padding=1)
+        self.bn2   = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32,  64,  3, padding=1)
+        self.bn3   = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64,  128, 3, padding=1)
+        self.bn4   = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 256, 3, padding=1)
+        self.bn5   = nn.BatchNorm2d(256)
         
-        self.fc1 = nn.Linear(64 * 6 * 6, 64)
-        self.fc2 = nn.Linear(64, 2)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.3)
+        
+        self.fc1 = nn.Linear(256 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, 2)
 
     def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = self.pool(torch.relu(self.conv3(x)))
+        x = self.pool(torch.relu(self.bn1(self.conv1(x))))
+        x = self.pool(torch.relu(self.bn2(self.conv2(x))))
+        x = self.pool(torch.relu(self.bn3(self.conv3(x))))
+        x = self.pool(torch.relu(self.bn4(self.conv4(x))))
+        x = self.pool(torch.relu(self.bn5(self.conv5(x))))
+        
         x = x.view(x.size(0), -1)
-        x = torch.relu(self.fc1(x))
+        x = self.dropout(torch.relu(self.fc1(x)))
         x = self.fc2(x)
         return x
 
@@ -92,7 +104,8 @@ for epoch in range(EPOCHS):
         optimizer.step()
         running_loss += loss.item()
 
-    print(f"Epoch {epoch+1}, Loss: {running_loss:.3f}")
+    epoch_loss = running_loss / len(train_loader)
+    print(f"Epoch {epoch+1}, Avg loss per batch: {epoch_loss:.4f}")
 
 correct = 0
 total = 0
